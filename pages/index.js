81ac90d549cc8459e857818ab904abcc9750f5a6
mbistami <mbistamimoussa@gmail.com>
2022-08-01 04:11:28 +0100

    fix login

--- pages/index.js
+++ pages/index.js
@@ -9,15 +9,15 @@ import LoadingScreen from "../components/LoadingScreen";
 export default function Home() {
   const router = useRouter();
   const [user, setUser] = useState();
-  let { loading, data } = useApi("long api");
+  let { loading, data } = useApi(
+    "https://api.veezen.com/api/v1/account/auth/getUserUsingJwt"
+  );
   useEffect(() => {
     setUser(data);
     router.prefetch("/home");
     if (user && user.isLogged == true) router.push("/dashboard");
-    else if (user && user.error == true) router.push("/login");
-    else router.push("/home");
   }, [loading, data, setUser, router, user]);
-  // if (!user) return <LoadingScreen />;
+  if (!user) return <LoadingScreen />;
   return (
     <>
       <div className={styles.container}></div>
