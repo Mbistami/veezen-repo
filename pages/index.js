8e22167588dcfc8080114707cc8a2a34eaf3707c
Moussa Bistami <mbistami@e2r8p1.1337.ma>
2022-08-02 17:53:41 +0100

    fix

--- pages/index.js
+++ pages/index.js
@@ -33,7 +33,10 @@ export default function Home() {
             if (res_.status === 200) {
               res_.json().then((data) => {
                 setUser({ ...data, Authorization: authorization });
-                localStorage.setItem({ ...data, Authorization: authorization });
+                localStorage.setItem("vee_user_data", {
+                  ...data,
+                  Authorization: authorization,
+                });
                 console.log("LOGGED IN REDIRECTION");
                 router.push("/dashboard");
               });
